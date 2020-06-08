aa5b5e5786011ec9a7a1267e851a51d02f2327a9
jianhengLiu <943678231@qq.com>
2020-06-09 00:37:21 +0800

    update simultaneous control demo

--- quadcopter_ws/src/flightController/src/flightController.cpp
+++ quadcopter_ws/src/flightController/src/flightController.cpp
@@ -90,6 +90,7 @@ Eigen::Vector4d PayloadController::getAllocatedRevs(double Force, Eigen::Vector3
 
 Eigen::Vector4d PayloadController::getRevs()
 {
+
     // 位置环 100Hz
     if(cnt>5 || first_time)
     {
@@ -163,7 +164,7 @@ Eigen::Vector4d PayloadController::getRevs()
     // 计算旋转误差 （10）
     Eigen::Vector3d errorRotationMatrix;
     errorRotationMatrix = 0.5 * antisymmetricMatrixToVector((desiredRotationMatrix.transpose() * rotationMatrix_BuW -
-                                                                                    rotationMatrix_BuW.transpose() * desiredRotationMatrix));
+                                                             rotationMatrix_BuW.transpose() * desiredRotationMatrix));
 
     // 计算角速度误差 (11)
     Eigen::Vector3d errorAngular =   cAngularVelocity_body -cDesiredWd;//小角度假设下可忽略： - rotationMatrix_BuW.transpose() * desiredRotationMatrix * desiredAngular;
@@ -176,11 +177,126 @@ Eigen::Vector4d PayloadController::getRevs()
     J_q<<5,5,5;
     Eigen::Vector3d nonlin;
     nonlin = (cAngularVelocity_body.cross(rotationMatrix_BuW.transpose()* desiredRotationMatrix * cDesiredWd)  -
-            rotationMatrix_BuW.transpose() * desiredRotationMatrix * cDesiredDotWd);
+              rotationMatrix_BuW.transpose() * desiredRotationMatrix * cDesiredDotWd);
 
     Eigen::Vector3d moment =
-    - errorRotationMatrix.cwiseProduct(k_p)
-    + errorAngular.cwiseProduct(k_d);
+            - errorRotationMatrix.cwiseProduct(k_p)
+            + errorAngular.cwiseProduct(k_d);
+//    + J_q.cwiseProduct(nonlin);
+
+//            保方向饱和函数
+//    if (moment.norm() > 5)
+//    {
+//        moment = 5 * moment / moment.norm();
+//    }
+
+
+
+    Eigen::Vector4d revs = getAllocatedRevs(u1, moment);
+
+    return revs;
+}
+
+Eigen::Vector4d PayloadController::getRevs(Eigen::Vector3d inputDesiredPos,
+                                           Eigen::Vector3d inputDesiredVel,
+                                           Eigen::Vector3d inputDesiredAcc,
+                                           Eigen::Vector3d inputDesiredJerk)
+{
+    // 位置环 100Hz
+    if(cnt>5 || first_time)
+    {
+        cDesiredPositionBody = inputDesiredPos;
+        cDesiredVelocityBody = inputDesiredVel;
+        dAccelerationBody  = inputDesiredAcc;
+        cDesiredJerk = inputDesiredJerk;
+        cDesiredPsi = 0;
+        cDesiredDotPsi = 0;
+
+        first_time = false;
+        cnt = 0;
+        // 计算误差 (2)
+        Eigen::Vector3d e_p = positionBody - cDesiredPositionBody;
+        Eigen::Vector3d e_v = cVelocityBody - cDesiredVelocityBody;
+//        std::cout<<e_p.transpose()<<" / "<<e_v.transpose()<<std::endl;
+        // 设置状态反馈误差增益 (3)
+        const Eigen::Vector3d K_p = Eigen::Vector3d(0.65, 0.65, 5);
+        const Eigen::Vector3d K_v = Eigen::Vector3d(1.7, 1.7, 5);
+
+        // 计算期望力矩（3）
+        Eigen::Vector3d zW(0, 0, -1);
+        Eigen::Vector3d F_n = -e_p.cwiseProduct(K_p) - e_v.cwiseProduct(K_v)
+                              +(massQuadcopter) * (9.8 * -zW ) + (massQuadcopter) * dAccelerationBody;;
+
+        Eigen::Vector3d F = F_n;
+//    保方向饱和函数
+//        if (F.norm() > 50)
+//        {
+//            F = 50 * F / F.norm();
+//        }
+
+        // 计算无人机拉力方向 （5）
+        Eigen::Vector3d zB_des = F / F.norm();
+        // 计算中间坐标系（7）
+        Eigen::Vector3d xC_des(cos(cDesiredPsi), sin(cDesiredPsi), 0);
+        // 计算yB和xB旋转 （8）
+        Eigen::Vector3d yB_des, xB_des;
+        yB_des = zB_des.cross(xC_des);
+        yB_des /= yB_des.norm();
+
+        xB_des = yB_des.cross(zB_des);
+
+        // 计算目标推力 （4）
+        u1 = zB_des.transpose() * F;
+
+
+        // 计算目标旋转矩阵 (9)
+//    Eigen::Matrix3d desiredRotationMatrix;
+        desiredRotationMatrix.col(0) = xB_des;
+        desiredRotationMatrix.col(1) = yB_des;
+        desiredRotationMatrix.col(2) = zB_des;
+
+
+        // 计算目标角速度 (12)
+//    Eigen::Vector3d zW;
+//    zW << 0, 0, 1;
+        Eigen::Vector3d hw = massQuadcopter / u1 * (cDesiredJerk  - (zB_des.dot(cDesiredJerk)) * zB_des);
+        double wx = - hw.dot(yB_des) ;
+        double wy =   hw.dot(xB_des) ;
+        double wz = cDesiredDotPsi * zW.dot(zB_des) ;
+        Eigen::Vector3d w_BW;
+        w_BW << wx, wy, wz;
+        pDesiredWd = cDesiredWd;
+        cDesiredWd = w_BW;
+        cDesiredDotWd = getVectorD(cDesiredDotWd,pDesiredWd);
+
+    }
+    cnt++;
+
+
+
+    // 姿态环 1kHz
+
+    // 计算旋转误差 （10）
+    Eigen::Vector3d errorRotationMatrix;
+    errorRotationMatrix = 0.5 * antisymmetricMatrixToVector((desiredRotationMatrix.transpose() * rotationMatrix_BuW -
+                                                             rotationMatrix_BuW.transpose() * desiredRotationMatrix));
+
+    // 计算角速度误差 (11)
+    Eigen::Vector3d errorAngular =   cAngularVelocity_body -cDesiredWd;//小角度假设下可忽略： - rotationMatrix_BuW.transpose() * desiredRotationMatrix * desiredAngular;
+
+
+    // 计算三轴动量
+    const Eigen::Vector3d k_p = Eigen::Vector3d(3, 3, 2);
+    const Eigen::Vector3d k_d = Eigen::Vector3d(0.8, 0.8, 1);
+    Eigen::Vector3d J_q;
+    J_q<<5,5,5;
+    Eigen::Vector3d nonlin;
+    nonlin = (cAngularVelocity_body.cross(rotationMatrix_BuW.transpose()* desiredRotationMatrix * cDesiredWd)  -
+              rotationMatrix_BuW.transpose() * desiredRotationMatrix * cDesiredDotWd);
+
+    Eigen::Vector3d moment =
+            - errorRotationMatrix.cwiseProduct(k_p)
+            + errorAngular.cwiseProduct(k_d);
 //    + J_q.cwiseProduct(nonlin);
 
 //            保方向饱和函数
